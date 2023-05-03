e2df0063bab0f3f27b1019ca5b7ed9264d89e8ea
mkostyk <m.kostyk22@gmail.com>
2023-05-03 14:07:24 +0200

    Added joining room logic

--- client/src/components/Room/Room.tsx
+++ client/src/components/Room/Room.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { socket } from "../App";
+
 function Room() {
+    let [playersInRoom, setPlayersInRoom] = useState<string[]>([]);
+
+    socket.on("new-player", (players: Array<string>) => {
+        setPlayersInRoom(players);
+    });
+
+    // Testing HTML
     return (
         <div>
             <h1>Room</h1>
+            <h2>Players in room:</h2>
+            <ul>
+                {playersInRoom.map((player, key) => (
+                    <li key={key}>{player}</li>
+                ))}
+            </ul>
         </div>
     )
 }
--- server/server.ts
+++ server/server.ts
@@ -1,37 +1,47 @@
-var io = require('socket.io')(8000, {
+const io = require('socket.io')(8000, {
     cors: {
         origin: '*',
     }
 });
-var playerRooms = new Map();
-var rooms = new Map();
-var nicknames = new Map(); // socket.id, nickname
-var currentRoomID = 0;
-io.on('connection', function (socket) {
+
+const playerRooms = new Map<string, number>();
+const rooms = new Map<number, string[]>();
+const nicknames = new Map<string, string>(); // socket.id, nickname
+let currentRoomID = 0;
+
+io.on('connection', socket => {
     console.log('connected');
-    socket.on('joining-room', function (nickname) {
+    
+    socket.on('joining-room', nickname => {
         console.log(nickname);
         nicknames.set(socket.id, nickname);
+
         if (playerRooms.get(socket.id) !== undefined) {
             // Already in a room.
             socket.emit('joined-room', playerRooms.get(socket.id));
             return;
         }
-        if (rooms.get(currentRoomID) === undefined || rooms.get(currentRoomID).length === 4) {
+
+        if (rooms.get(currentRoomID) === undefined || rooms.get(currentRoomID)!.length === 4) {
             // Creating new room.
             currentRoomID++;
             rooms.set(currentRoomID, []);
         }
+
         // Joining room.
         socket.join(currentRoomID);
-        rooms.get(currentRoomID).push(socket.id);
+        rooms.get(currentRoomID)!.push(socket.id);
         playerRooms.set(socket.id, currentRoomID);
+
         console.log(rooms.get(currentRoomID));
         socket.emit('joined-room', currentRoomID);
+        
         // Informing players of new player.
-        io.in(currentRoomID).emit('new-player', rooms.get(currentRoomID).map(function (id) { return nicknames.get(id); }));
+        io.in(currentRoomID).emit('new-player', rooms.get(currentRoomID)!.map(id => nicknames.get(id)));
     });
-    socket.on('leaving-room', function () {
+
+
+    socket.on('leaving-room', () => {
         console.log('leaving-room');
     });
 });
