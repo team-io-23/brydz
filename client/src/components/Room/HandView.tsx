e5a11acb15338d64889b9f06852aceac3f389ac4
mkostyk <m.kostyk22@gmail.com>
2023-05-03 19:56:41 +0200

    Upgraded styles.css

--- client/src/components/Room/HandView.tsx
+++ client/src/components/Room/HandView.tsx
@@ -3,7 +3,7 @@ function HandView() {
     var playerHand: [string, string, string][] = [["a", "clubs", "♣"], ["k", "spades", "♠"], ["q", "hearts", "♥"], ["j", "diams", "♦"], ["10", "clubs", "♣"], ["9", "spades", "♠"], ["8", "hearts", "♥"], ["7", "diams", "♦"], ["6", "clubs", "♣"], ["5", "spades", "♠"], ["4", "hearts", "♥"], ["3", "diams", "♦"], ["2", "clubs", "♣"]];
     return (
         <div className="playingCards faceImages">
-            <ul className="table">
+            <ul className="hand">
                 {playerHand.map(([card, suit, symbol], index) => (
                     <li>
                         <a className={`card rank-${card} ${suit}`}>
