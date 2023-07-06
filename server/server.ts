38df1a2548fe406a369e93e4221914075971650f
mkostyk <m.kostyk22@gmail.com>
2023-07-06 22:35:59 +0200

    Some random test

--- server/server.ts
+++ server/server.ts
@@ -4,10 +4,11 @@ import { Bid, Card, Score, Hand, PlayedCard } from './types';
 import express from 'express';
 
 const PORT = 8000;
-const INDEX = '/';
+const INDEX = '../client/public/index.html';
 
 const server = express()
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = require('socket.io')(server, {
     cors: {
