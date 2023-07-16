9c0f6d339a93d8725ca54b09fc9ed54f1a36a01b
Maciej Skórski <maciej.skorski@gmail.com>
2023-07-16 20:30:27 +0000

    fixed tests

--- tests/selenium/test01.py
+++ tests/selenium/test01.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,27 +7,33 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # Join menu test
 
-driver = webdriver.Firefox()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
 
 driver.get("http://localhost:3000")
 
+# Step 1: sign in
 name_input = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, ":r1:"))
 )
-
 name_input.send_keys("John")
-
 
 join_button = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.CSS_SELECTOR, ".StartPage-JoinButton"))
 )
-
 join_button.click()
 
+# Step 2: create a room
+create_room_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, ".create-room-button"))
+)
+create_room_button.click()
+
 try:
-    WebDriverWait(driver, 10).until(EC.url_to_be('http://localhost:3000/waitingRoom'))
-    assert driver.current_url == 'http://localhost:3000/waitingRoom', "Redirection to waiting room failed"
+    WebDriverWait(driver, 10).until(EC.url_to_be('http://localhost:3000/#/waitingRoom'))
+    assert driver.current_url == 'http://localhost:3000/#/waitingRoom', "Redirection to waiting room failed"
 except AssertionError as e:
-    print(e)
+    raise(e)
 finally:
     driver.quit()
